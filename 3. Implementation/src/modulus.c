53b79554002a583338384b49df4a31d198f259d4
Stepin104886 <69183173+Mohammad-Fayaz-01@users.noreply.github.com>
2021-02-05 17:45:32 +0530

    Create modulus.c

---
+++ 3. Implementation/src/modulus.c
@@ -0,0 +1,11 @@
+#include "header.h"
+void modulus()
+{
+    int a, b, d=0; 
+    printf("\nPlease enter first number   : "); 
+    scanf("%d", &a); 
+    printf("Please enter second number  : "); 
+    scanf("%d", &b);
+    d=a%b;
+    printf("\nModulus of entered numbers = %d\n",d);
+}
