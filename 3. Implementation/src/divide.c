39aef10ec1c8700d56f460a64201f76d835b6a7b
99003572 <78578826+99003572@users.noreply.github.com>
2021-02-05 17:21:12 +0530

    Update divide.c

--- 3. Implementation/src/divide.c
+++ 3. Implementation/src/divide.c
@@ -1,1 +1,10 @@
-
+#include "header.h"
+void divide_numbers()
+{
+    int num1, num2;
+    printf(" Enter the first number\n");
+    scanf("%d", &num1);
+    printf(" Enter the second number\n");
+    scanf("%d", &num2);
+    printf("%d / %d = %d\n",num1,num2,num1/num2);
+}
