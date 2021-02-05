614689dc1e8ebd68e82d3f1c26d9bc93c557f38d
99003572 <78578826+99003572@users.noreply.github.com>
2021-02-05 17:20:31 +0530

    Update multiply.c

--- 3. Implementation/src/multiply.c
+++ 3. Implementation/src/multiply.c
@@ -1,1 +1,10 @@
-
+#include "header.h"
+void multiply_numbers()
+{
+    int num1, num2;
+    printf(" Enter the first number\n");
+    scanf("%d", &num1);
+    printf(" Enter the second number\n");
+    scanf("%d", &num2);
+    printf("%d x %d = %d\n",num1,num2,num1*num2);
+}
